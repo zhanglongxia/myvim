dde05bf3eef3acbc38c8a185ecac7cb6735c0893
Zhanglong Xia <zhanglongxia@google.com>
2024-08-01 18:01:28 +0800

    add a script to install vimrc

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 This repo contains a vim configuration.
+
+Install the vim configuration:
+
+```
+$ cd <path-to-myvim>
+$ ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+VIM_DIR="${HOME}/.vim"
+
+init()
+{
+    vim_dir="${HOME}/restore/.vim"
+    if [ -d "${vim_dir}" ]; then
+        rm -rf "${vim_dir}"
+    fi
+
+    vim_dir="${HOME}/.vim"
+    if [ -d "${vim_dir}" ]; then
+        rm -rf "${vim_dir}"
+    fi
+
+    vimrc="${HOME}/.vimrc"
+    if [ -f "${vimrc}" ]; then
+        rm -rf "${vimrc}"
+    fi
+}
+
+install_vimrc()
+{
+    mkdir "${VIM_DIR}"
+    mkdir "${VIM_DIR}/colors"
+    mkdir "${VIM_DIR}/plugged"
+    mkdir "${VIM_DIR}/autoload"
+    cp  ./linux/vimrc "${VIM_DIR}"
+    ls "${VIM_DIR}"
+}
+
+install_tools()
+{
+    sudo apt-get install -y curl cscope cmake clang exuberant-ctags
+    curl -fLo "${VIM_DIR}/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    ls "${VIM_DIR}"
+}
+
+install_colors()
+{
+    cd "${VIM_DIR}"
+    git clone git@github.com:altercation/solarized.git --depth=1
+    cp "${VIM_DIR}/solarized/vim-colors-solarized/colors/solarized.vim" "${VIM_DIR}/colors/"
+    rm -rf "${VIM_DIR}/solarized"
+}
+
+install_ycm()
+{
+    cd "${VIM_DIR}/plugged"
+    if [ ! -d "${VIM_DIR}/plugged/YouCompleteMe" ]; then
+        git clone https://github.com/ycm-core/YouCompleteMe.git
+    fi
+
+    cd "${VIM_DIR}/plugged/YouCompleteMe"
+    git submodule update --init --recursive
+    ./install.py --clang-completer
+}
+
+main()
+{
+    init
+    install_vimrc
+    install_tools
+    install_colors
+    install_ycm
+}
+
+main
